c151b72b695bb19007f3e2c45088995f4c02a419
kimberlinn99 <alisa.uth99@gmail.com>
2021-02-04 22:02:58 +0700

    Style: Add constant variables and logo

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFFAEE2FF);
+const kSecondaryColor = Color(0xFF6FBAE3);
+const kGreenColor = Color(0xFF5EA732);
+const kRedColor = Color(0xFFFF4242);
+const kDefaultPadding = 20;
+
+ThemeData themeData = ThemeData(
+  scaffoldBackgroundColor: kPrimaryColor,
+);
